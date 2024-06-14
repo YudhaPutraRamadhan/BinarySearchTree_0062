eae4abf79347b0b83012b15293d1b61f4cb772c0
MzSuwawa <yudharamadhan838@gmail.com>
2024-06-14 13:51:10 +0700

    Menambahkan perkondisian if jika nilai di bidang data node baru kurang dari nilai di bidang data, left child dari parent akan menunjuk ke node baru

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -40,5 +40,9 @@ public:
             ROOT = newNode; 
             return; 
         }
-    }
+
+        if (element < parent->info) {
+            parent->leftchild = newNode;
+        }
+        
 };
